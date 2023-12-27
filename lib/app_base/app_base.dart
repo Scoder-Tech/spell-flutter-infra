f7e2c8d9d997b9437f144d21754c4d3734027f6d
Lucas <devlucasteles@gmail.com>
2023-12-27 10:57:08 -0300

    Add app_base library and related files

---
+++ lib/app_base/app_base.dart
@@ -0,0 +1,4 @@
+library app_base;
+
+export 'web_scroll_behavior.dart';
+export 'custom_network_file_translator.dart';
--- lib/flutter_infra_scoder.dart
+++ lib/flutter_infra_scoder.dart
@@ -3,3 +3,4 @@ library flutter_infra_scoder;
 export 'extensions/extensions.dart';
 export 'http/http.dart';
 export 'responsivity/form_factor.dart';
+export 'app_base/app_base.dart';
