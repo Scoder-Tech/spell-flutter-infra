6a16fdb6fee58b3f190798ae92febb0d92deac06
Samuel <samuel@brandani.net.br>
2023-12-29 18:36:01 -0300

    add extensions to string and date parsing

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -3,6 +3,7 @@ library extensions;
 export 'date/date_extension.dart';
 export 'helper_extensions/color_extensions.dart';
 export 'helper_extensions/string_extensions.dart';
+export 'money/money_extension.dart';
 export 'numbers/num_extension.dart';
 export 'spacement/padding_extension.dart';
 export 'spacement/responsive.dart';
