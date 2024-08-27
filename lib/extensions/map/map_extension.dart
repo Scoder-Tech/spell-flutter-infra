e025cd889318a6fd84bf3d0d9b0fdc8bb07f32e7
Filipe <filipefcl@hotmail.com>
2024-08-27 19:42:35 -0300

    fix: intl version

--- lib/extensions/map/map_extension.dart
+++ lib/extensions/map/map_extension.dart
@@ -3,6 +3,6 @@
 extension RemoveNull on Map<String, dynamic> {
   Map<String, dynamic> removeNull() {
     return Map.fromEntries(entries
-        .where((element) => element.value != null && element.value != ''));
+        .where((element) => element.value != null && element.value != '' && element.value != 'null'));
   }
 }
